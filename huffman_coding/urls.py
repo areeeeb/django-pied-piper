9897b913feeacdac883d83e93dc67a2bd1e5701d
Areeb Ahmed <areeb_aaa@outlook.com>
2020-01-05 23:46:40 +0500

    decompression left

--- huffman_coding/urls.py
+++ huffman_coding/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('text_compress/', views.text_compress_view, name='text-compress'),
     path('image_compress/', views.image_compress_view, name='image-compress'),
+    path('file_download/<file_name>/', views.download_file_view, name='download-file'),
 ]
--- huffman_coding/views.py
+++ huffman_coding/views.py
@@ -1,7 +1,7 @@
 import pickle
 
 from django.shortcuts import render, redirect
-from PIL import Image
+from django.http import FileResponse, HttpResponse
 
 from script.HuffmanTree import HuffmanTree
 from script.helper_functions import get_key
@@ -24,10 +24,21 @@ def create_tree_and_save_files(huffman_tree_obj):
               f'Projects\\django_pied_piper\\media\\compressed'
               f'\\{key}.bin', 'wb') as fp:
         fp.write(bit_array)
+    return f'{key}.bin'
 
 
 def home_view(request):
     return render(request, 'huffman_coding/home.html')
+
+
+def download_file_view(request, file_name):
+    with open(f'C:\\Users\\areeb\\Documents\\Programming\\Pycharm '
+              f'Projects\\django_pied_piper\\media\\compressed'
+              f'\\{file_name}', 'rb') as fp:
+        response = HttpResponse(fp.read(),
+                                content_type='application/octet-stream')
+        response['Content-Disposition'] = 'attachment;filename=compressed.bin'
+        return response
 
 
 def text_compress_view(request):
@@ -39,10 +50,9 @@ def text_compress_view(request):
             text = str(text, 'utf-8')
 
             huffman_tree = HuffmanTree(data=text)
-            create_tree_and_save_files(huffman_tree)
+            file_name = create_tree_and_save_files(huffman_tree)
 
-            # TODO: redirect to file download page
-            return redirect('home')
+            return redirect('download-file', file_name)
     else:
         form = forms.TextFileForm()
     return render(request, 'huffman_coding/compress.html', {'form': form})
@@ -53,13 +63,17 @@ def image_compress_view(request):
         form = forms.ImageFileForm(request.POST, request.FILES)
         if form.is_valid():
             fp = request.FILES['image_file']
-            with open(r'C:\Users\areeb\Documents\Programming\Pycharm Projects\django_pied_piper\media\temp\temp.bmp', 'wb') as temp_fp:
+            with open(
+                    r'C:\Users\areeb\Documents\Programming\Pycharm Projects\django_pied_piper\media\temp\temp.bmp',
+                    'wb') as temp_fp:
                 temp_fp.write(fp.read())
-            huffman_tree = HuffmanTree(file_path=r'C:\Users\areeb\Documents\Programming\Pycharm Projects\django_pied_piper\media\temp\temp.bmp', is_image=True)
-            create_tree_and_save_files(huffman_tree)
+            huffman_tree = HuffmanTree(
+                file_path=r'C:\Users\areeb\Documents\Programming\Pycharm Projects\django_pied_piper\media\temp\temp.bmp',
+                is_image=True)
+            file_name = create_tree_and_save_files(huffman_tree)
 
-            # TODO: redirect to file download page
-            return redirect('home')
+            return redirect('download-file', file_name)
+
     else:
         form = forms.ImageFileForm()
     return render(request, 'huffman_coding/compress.html', {'form': form})
